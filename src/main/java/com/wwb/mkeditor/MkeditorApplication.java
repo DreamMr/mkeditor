977146896baba0e9ad56daa670106a4d57402694
网之管 <qc_d434a8648c0e41ce9b5dc306a61ce3ae@git.code.tencent.com>
2019-05-12 20:57:43 +0800

    创建仓库

---
+++ src/main/java/com/wwb/mkeditor/MkeditorApplication.java
@@ -0,0 +1,13 @@
+package com.wwb.mkeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkeditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkeditorApplication.class, args);
+    }
+
+}
